32fc3b7bef4eae3f07a25a3a9b5fec7eb70a2502
wxcsdb88 <wxcsdb88@gmail.com>
2018-08-08 15:26:55 +0800

    add api and fixed log

---
+++ api/handlers/base.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/wxcsdb88/gin-quick/api/common"
+)
+
+// Ping return without content
+func Ping() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		log.Debug("ping %v", time.Now().Local())
+		responseData := common.NewResponseData(200, "server is ok", nil, c.Request.RequestURI)
+		ResponseJSON(c, responseData)
+	}
+}
